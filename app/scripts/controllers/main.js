dfe16657dd105f2ab3c5115289279ee71d745022
Alexandre Nunesse <anunesse@excilys.com>
2014-10-10 13:43:45 +0200

    Setup Project : yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('userDatabaseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
