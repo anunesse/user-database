3b6951f4df81654b41cb878807b9ae422e7dc7bd
Alexandre Nunesse <anunesse@excilys.com>
2014-10-10 15:26:42 +0200

    Add user info and PRACTICE comments

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,21 @@
 
 angular.module('userDatabaseApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.users = [
+      {
+      	'firstname':'Alex',
+      	'lastname':'Nunes',
+      	'age':'23'
+      },
+      {
+      	'firstname':'Loic',
+      	'lastname':'Ortola',
+      	'age':'25'
+      },
+      {
+      	'firstname':'John',
+      	'lastname':'Doe',
+      	'age':'33'
+      }
     ];
   });
